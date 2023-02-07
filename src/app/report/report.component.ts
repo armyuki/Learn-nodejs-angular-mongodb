71e1ba4029414c6ce3dec7187fc108381d9b00ed
YUKIz <47018763+armyuki@users.noreply.github.com>
2023-02-08 02:27:45 +0700

    Update report.component.ts

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ShareService } from '../ShareService';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.css']
 })
-export class ReportComponent {
+export class ReportComponent implements OnInit {
+
+  healths: any;
+  totalPrice = 0;
+
+  search = {
+    from: '',
+    to: '',
+  }
+
+  constructor(private http: HttpClient, private ShareService: ShareService) { }
+
+  ngOnInit() {
+
+  }
+
+  showReport() {
+    console.log(this.search);
+    this.http.post(this.ShareService.serverpath + '/showreport', this.search).subscribe((res: any) => {
+      this.healths = res
+
+      for(let i = 0; i < res.length; i++) {
+        this.totalPrice += res[i].price
+      }
+    })
+
+  }
 
 }
